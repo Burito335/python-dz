3dc74f63e8b209e463d86d1cf2424a5ec7d1ca1d
Burito335 <olegsmereka220@gmail.com>
2024-11-30 14:26:28 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,81 @@
+# Тип змінної int, який містить цілі числа
+# number = 10
+
+# Тип змінної float, який містить не цілі числа
+# price = 10.2
+
+# Тип змінної String, який містить текст/рядок
+# name = "Oleghghg"
+
+# Тип змінної bool (boolean), який містить True або False
+# light = True
+
+# student = 11
+# price_shop = 11.00
+# table = "Hello World"
+# live = True
+
+
+# print("Мені",name)
+
+# name = 'huk'
+# age = 38
+# iq = 63.6
+# smart = False
+
+# print('my name is', name,'I am',age,'My IQ is',iq,'I am smart', smart)
+# x = 1
+# y = 2.566785347
+# nick = 'kjkjljkkjllkjkkj'
+# work = True
+# print(x +y )
+# sum = x+7
+# print(sum)
+# if x  ==  y:
+#     print('bnfds')
+# else:
+#     print('popopopopop')
+
+# x  = 30
+# y = 40
+# b = 16
+# c = 67
+# a =  78
+# d = x +y*c/b-a 
+
+# if d > 20:
+#     if x <39:
+#        print('eror')
+#     else:
+#         print('it step')
+#     print ("ok")
+# elif a > 10:
+#     print('ne ok')
+# elif d ==8:
+#     print ('ok')
+# else :
+#     print('ne ok')
+  
+
+
+# x = 29
+# for i in range(10):
+#     print(x)
+
+# b = 0
+# while b < 10:
+#     b+= 1
+#     print(b)
+# x = "Hello world"
+# for i in range(50):
+#       print(x)
+# while x =="Hello world" :
+      
+#       print (x)
+
+
+def fun():
+    a = 5
+    b = 6
+    print(a-b)
+fun()
